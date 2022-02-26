39dba2b4d43bdae1861e432fbca8b0d5e92a8f5d
pantherbots2021 <darcy_collins@dpsk12.org>
2022-02-26 13:07:22 -0700

    Created drivebase

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,85 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends TimedRobot {
+  private final PWMSparkMax m_leftDriveFront = new PWMSparkMax(1);
+  private final PWMSparkMax m_leftDriveBack = new PWMSparkMax(2);
+  private final PWMSparkMax m_rightDriveFront = new PWMSparkMax(5);
+  private final PWMSparkMax m_rightDriveBack = new PWMSparkMax(6); 
+
+  private final MotorControllerGroup leftDriveGroup = new MotorControllerGroup(m_leftDriveFront, m_leftDriveBack);
+  private final MotorControllerGroup rightDriveGroup = new MotorControllerGroup(m_rightDriveFront, m_rightDriveBack);
+
+
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(leftDriveGroup, rightDriveGroup);
+  private final GenericHID m_stick = new GenericHID(0);
+  private final Timer m_timer = new Timer();
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_robotDrive.arcadeDrive(-m_stick.getRawAxis(0), m_stick.getRawAxis(1));
+
+    leftDriveGroup.setInverted(false);
+    rightDriveGroup.setInverted(true);
+  }
+
+  /** This function is run once each time the robot enters autonomous mode. */
+  @Override
+  public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    // Drive for 2 seconds
+    if (m_timer.get() < 2.0) {
+      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+    } else {
+      m_robotDrive.stopMotor(); // stop robot
+    }
+  }
+
+  /** This function is called once each time the robot enters teleoperated mode. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during teleoperated mode. */
+  @Override
+  public void teleopPeriodic() {
+    m_robotDrive.arcadeDrive(-m_stick.getRawAxis(0), m_stick.getRawAxis(1));
+  }
+
+  /** This function is called once each time the robot enters test mode. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
