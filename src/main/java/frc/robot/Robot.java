a39dd818ab908cecde6a2e7f003eb1003259482d
Dresden6 <72036931+Dresden6@users.noreply.github.com>
2022-03-23 13:48:41 -0600

    Reverted Code to correct version

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,20 +6,20 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or 
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the manifest file in the resource
  * directory.
  */
@@ -28,7 +28,8 @@ public class Robot extends TimedRobot {
   private final CANSparkMax m_leftDriveBack = new CANSparkMax(2, MotorType.kBrushless);
   private final CANSparkMax m_rightDriveFront = new CANSparkMax(5, MotorType.kBrushless);
   private final CANSparkMax m_rightDriveBack = new CANSparkMax(6, MotorType.kBrushless); 
-  private final CANSparkMax m_  = new CANSparkMax(9, MotorType.kBrushless);
+  private final CANSparkMax m_MotorArm = new CANSparkMax(9, MotorType.kBrushless);
+  private final VictorSPX m_IntakeArm = new VictorSPX(10);
 
 
  
@@ -66,9 +67,14 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     // Drive for 2 seconds
     if (m_timer.get() < 2.0) {
-      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+      m_robotDrive.arcadeDrive(0, 0.5); // drive forwards half speed
+
+      
     } else {
       m_robotDrive.stopMotor(); // stop robot
+
+      m_timer.stop();
+      
     }
   }
   // Test comment
@@ -80,9 +86,33 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(-m_stick.getRawAxis(1), m_stick.getRawAxis(2));
+    m_robotDrive.arcadeDrive(m_stick.getRawAxis(4) / 2, -m_stick.getRawAxis(1));
    
 
+    if(m_stick.getRawButton(5)){
+
+      m_IntakeArm.set(ControlMode.PercentOutput, -.5);
+    } 
+    else if(m_stick.getRawButton(6)){
+
+      m_IntakeArm.set(ControlMode.PercentOutput, .5);
+    } else {
+      m_IntakeArm.set(ControlMode.PercentOutput, 0);
+
+    }
+
+
+    if(m_stick.getRawAxis(2) > .2){
+
+      m_MotorArm.set(1);
+    } 
+    else if(m_stick.getRawAxis(3) > .2){
+
+      m_MotorArm.set(-.7);
+    } else {
+      m_MotorArm.set(0);
+
+    }
   
     SmartDashboard.putNumber("Left Y", m_stick.getRawAxis(1));
     SmartDashboard.putNumber("Right X", m_stick.getRawAxis(2));
